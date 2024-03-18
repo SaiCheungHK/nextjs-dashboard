c7bf758cdf4c1053cecf4d4bf088417ade5031bc
SaiCheungHK <75961372+SaiCheungHK@users.noreply.github.com>
2024-03-18 16:33:30 +0800

    Chapter 4-5

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,7 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.scss'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
